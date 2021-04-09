03344d71e2a2b78b7db4279e886623f8373e604a
tsahiBarshevsky <tsahi.13@gmail.com>
2021-04-09 12:43:23 +0300

    Hero and footer done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer()
 {
     return (
-        <div className="footer-container">
+        <div className="footer-container" title="Photo by Janko Ferlič from Unsplash">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#f5f5f5" fill-opacity="1" d="M0,192L34.3,186.7C68.6,181,137,171,206,149.3C274.3,128,343,96,411,106.7C480,117,549,171,617,192C685.7,213,754,203,823,176C891.4,149,960,107,1029,117.3C1097.1,128,1166,192,1234,202.7C1302.9,213,1371,171,1406,149.3L1440,128L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path>
+                <path fill="#f5f5f5" fillOpacity="1" d="M0,128L40,149.3C80,171,160,213,240,202.7C320,192,400,128,480,112C560,96,640,128,720,160C800,192,880,224,960,213.3C1040,203,1120,149,1200,133.3C1280,117,1360,139,1400,149.3L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
             </svg>
             <div className="footer-content">
-                <div className="text">
-                    נתיב להצלחה &bull; כל הזכויות שמורות &copy; {new Date().getFullYear()}
-                </div>
+                <p>
+                    נתיב להצלחה &bull; כל החומרים הינם מקוריים
+                    <br/>
+                    כל הזכויות שמורות לצחי בארשבסקי &copy; {new Date().getFullYear()}
+                </p>
             </div>
         </div>
     )
